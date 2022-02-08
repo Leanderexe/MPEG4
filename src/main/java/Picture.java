8cf80cfd9f907d7aa8644ecbf3e2ac0c221d1dd1
LeanderHermanns <leander.hermanns@t-online.de>
2022-02-08 02:24:17 +0100

    Improvement of the GUI

--- src/main/java/Picture.java
+++ src/main/java/Picture.java
@@ -7,23 +7,41 @@ public class Picture {
     public void build(){
         // first Frame.
         for (int i = 0; i < 10; i++) {
-            double random = new Random().nextDouble();
-            double random2 = new Random().nextDouble();
+            List Canvas = new ArrayList();
+            double random_int1 = new Random().nextInt(2) - 1; // Create a random integer number between -1 and 0.
+            double random_int2 = new Random().nextInt(2) - 1;
+            double random_int3 = new Random().nextInt(2) - 1;
+            double random_int4 = new Random().nextInt(2) - 1;
+            double random = new Random().nextDouble() + random_int1; // Creates a random number between -1 and 1.
+            double random2 = new Random().nextDouble() + random_int2;
+            double random3 = new Random().nextDouble() + random_int3;
+            double random4 = new Random().nextDouble() + random_int4;
             VideoFrame c = new VideoFrame(32, 32, "+");
             List array = new ArrayList();
             array.add(random);
             array.add(random2);
             array.add(0.);
+            List array2 = new ArrayList();
+            array2.add(random3);
+            array2.add(random4);
+            array2.add(0.);
             Sphere l = new Sphere(array, 0.5, "A");
+            Sphere l2 = new Sphere(array2, 0.7, "B");
             c.attach(l);
+            c.attach(l2);
             //System.out.print(Color.BLUE_BOLD);
             Window win = new Window();
-            win.build_blocks(8, 32, c.get_Canvas());
+            win.build_blocks(8, 32, c.get_Canvas(), "P");
+            Canvas.add(c);
             //win.build_window(c.render());
-            //System.out.println(c.render());
-
-
-
+            System.out.println(c.render());
+            //Scanner in = new Scanner(System.in);
+            //String str = in.nextLine();
+            /*
+            for (int p = 0; p < Canvas.size(); p++){
+                VideoFrame vid = (VideoFrame) Canvas.get(p);
+                System.out.println("Hellopepe" + vid.render());
+            } */
         }
 
         // second Frame.
--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,15 +1,23 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.event.AncestorListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Window {
 
+    JFrame window = new JFrame();
     private JTextArea b1;
 
-    public void build_window(String frame){
+    public void build_window(String frame, String frametype){
         JFrame window = new JFrame();
-        window.setTitle("MPEG4");
+        window.setTitle("MPEG4" + frametype);
         window.setSize(1000, 1000);
         window.setLocation(500, 500);
         //window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,101 +39,126 @@ public class Window {
         window.add(area, BorderLayout.SOUTH);
     }
 
-    public void build_blocks(int blocksize, int canvassize, String[][] canvas){
-        List Blocks = new ArrayList();
+    public void build_blocks(int blocksize, int canvassize, String[][] canvas, String frametype){
+        String[][] Blocks = new String[canvassize][canvassize];
         for (int i = 0; i < 4; i++) {
             for (int p = 0; p < 4; p++) {
                 String Block = "";
                 for (int j = 8 * i; j < 8 * i + blocksize; j++) {
                     for (int n = 8 * p; n < 8 * p + blocksize; n++) {
-                        System.out.println("hihihihgffffffffffffffffffff");
-                        Block += canvas[j][n];
+                        if (canvas[j][n].equals("+")){
+                            Blocks[j][n] = "0x051BF7";
+                        }
+                        if (canvas[j][n].equals("A")){
+                            Blocks[j][n] = "0xE74C3C";
+                        }
+                        if (canvas[j][n].equals("B")){
+                            Blocks[j][n] = "0xF7CB05";
+                        }
+                        //System.out.println("hihihihgffffffffffffffffffff");
+                        //Block += canvas[j][n];
                     }
-                    Block += "\n";
+                    //Block += "\n";
                 }
-                Blocks.add(Block);
-                System.out.println(Block);
+                //Blocks.add(Block);
+                //System.out.println(Block);
             }
         }
         System.out.println(canvas.toString());
         System.out.println(Blocks);
-        build_blocks(Blocks);
+        build_blocks(Blocks, frametype);
     }
 
-    public void build_blocks(List blocks){
-        JFrame window = new JFrame();
-        window.setTitle("MPEG4");
-        window.setSize(1000, 1000);
-        window.setLocation(500, 500);
-        //window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public void build_blocks(String[][] blocks, String frametype){
+        //JFrame window = new JFrame();
+        window.setTitle("MPEG4(" + frametype + ")");
+        window.setSize(1250, 1250);
+        window.setLocation(800, 100);
         window.setVisible(true);
-        JPanel contentPane = new JPanel();
-        contentPane.setLayout(new FlowLayout(FlowLayout.RIGHT, 1, 1));
-        System.out.println(blocks.size());
-        JTextArea b1= new JTextArea((String) blocks.get(0));
-        b1.setBackground(Color.RED);
-        b1.setColumns(1);
-        JTextArea b2=new JTextArea((String) blocks.get(1));
-        b2.setBackground(Color.BLACK);
-        JTextArea b3=new JTextArea((String) blocks.get(2));
-        b3.setBackground(Color.BLUE);
-        JTextArea b4=new JTextArea((String) blocks.get(3));
-        b4.setBackground(Color.CYAN);
-        JTextArea b5=new JTextArea((String) blocks.get(4));
-        b5.setBackground(Color.GREEN);
-        JTextArea b6=new JTextArea((String) blocks.get(5));
-        b6.setBackground(Color.MAGENTA);
-        JTextArea b7=new JTextArea((String) blocks.get(6));
-        b7.setBackground(Color.DARK_GRAY);
-        JTextArea b8=new JTextArea((String) blocks.get(7));
-        b8.setBackground(Color.YELLOW);
-        JTextArea b9=new JTextArea((String) blocks.get(8));
-        b9.setBackground(Color.ORANGE);
-        JTextArea b10=new JTextArea((String) blocks.get(9));
-        b10.setBackground(Color.white);
-        JTextArea b11=new JTextArea((String) blocks.get(10));
-        b11.setBackground(Color.gray);
-        JTextArea b12=new JTextArea((String) blocks.get(11));
-        b12.setBackground(Color.PINK);
-        JTextArea b13=new JTextArea((String) blocks.get(12));
-        b13.setBackground(Color.lightGray);
-        JTextArea b14=new JTextArea((String) blocks.get(13));
-        b14.setBackground(Color.cyan);
-        JTextArea b15=new JTextArea((String) blocks.get(14));
-        b15.setBackground(Color.orange);
-        JTextArea b16=new JTextArea((String) blocks.get(15));
-        b16.setBackground(Color.green);
-        // b16.setForeground(Color.green);
-        /*
-        JTextArea b17=new JTextArea((String) blocks.get(16));
-        JTextArea b18=new JTextArea((String) blocks.get(17));
-        JTextArea b19=new JTextArea((String) blocks.get(18));
-        JTextArea b20=new JTextArea((String) blocks.get(19));
-        JTextArea b21=new JTextArea((String) blocks.get(20));
-        JTextArea b22=new JTextArea((String) blocks.get(21));
-        JTextArea b23=new JTextArea((String) blocks.get(22));
-        JTextArea b24=new JTextArea((String) blocks.get(23));
-        JTextArea b25=new JTextArea((String) blocks.get(24));
-        JTextArea b26=new JTextArea((String) blocks.get(25));
-        JTextArea b27=new JTextArea((String) blocks.get(26));
-        JTextArea b28=new JTextArea((String) blocks.get(27));
-        JTextArea b29=new JTextArea((String) blocks.get(28));
-        JTextArea b30=new JTextArea((String) blocks.get(29));
-        JTextArea b31=new JTextArea((String) blocks.get(30));
-        JTextArea b32=new JTextArea((String) blocks.get(31));
-
-         */
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
 
 
-        window.add(b1); window.add(b2); window.add(b3);
-        window.add(b4); window.add(b5); window.add(b6);
-        window.add(b7); window.add(b8);window.add(b9); window.add(b10); window.add(b11);
-        window.add(b12); window.add(b13); window.add(b14);
-        window.add(b15); window.add(b16);
+        JTextArea b1= new JTextArea();
+        build_pixel(blocks, b1, 0);
+        JTextArea b2=new JTextArea();
+        build_pixel(blocks, b2, 1);
+        JTextArea b3=new JTextArea();
+        build_pixel(blocks, b3, 2);
+        JTextArea b4=new JTextArea();
+        build_pixel(blocks, b4, 3);
+        JTextArea b5=new JTextArea();
+        build_pixel(blocks, b5, 4);
+        JTextArea b6=new JTextArea();
+        build_pixel(blocks, b6, 5);
+        JTextArea b7=new JTextArea();
+        build_pixel(blocks, b7, 6);
+        JTextArea b8=new JTextArea();
+        build_pixel(blocks, b8, 7);
+        JTextArea b9=new JTextArea();
+        build_pixel(blocks, b9, 8);
+        JTextArea b10=new JTextArea();
+        build_pixel(blocks, b10, 9);
+        JTextArea b11=new JTextArea();
+        build_pixel(blocks, b11, 10);
+        JTextArea b12=new JTextArea();
+        build_pixel(blocks, b12, 11);
+        JTextArea b13=new JTextArea();
+        build_pixel(blocks, b13, 12);
+        JTextArea b14=new JTextArea();
+        build_pixel(blocks, b14, 13);
+        JTextArea b15=new JTextArea();
+        build_pixel(blocks, b15, 14);
+        JTextArea b16=new JTextArea();
+        build_pixel(blocks, b16, 15);
+
+
+        for (JTextArea jTextArea : Arrays.asList(b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16)) {
+            jTextArea.setBorder(border);
+            jTextArea.setBackground(Color.BLACK);
+            window.add(jTextArea);
+        }
+
+        Action Enter = new EnterAction();
+        b1.getInputMap().put(KeyStroke.getKeyStroke("ENTER"), "EnterAction");
+        b1.getActionMap().put("EnterAction", Enter);  // Perform an action when Enter Key is pressed.
+        //public void EnterAction(){
 
         window.setLayout(new GridLayout(4,4));
-        window.setSize(300,600);
         window.setVisible(true);
 
+    }
+
+    public void build_pixel(String[][] blocks, JTextArea area, int blocknumber){
+        int blocksize = 8;
+        int i = (int) Math.floor(blocknumber/4);
+        int p = blocknumber%4;
+        System.out.println(p + " " + i);
+        area.setLayout(new GridLayout(8,8));
+        for (int j = 8 * i; j < 8 * i + blocksize; j++) {
+            for (int n = 8 * p; n < 8 * p + blocksize; n++) {
+                Border border = BorderFactory.createLineBorder(Color.GRAY);
+                JTextArea h1 = new JTextArea();
+                h1.setBorder(border);
+                h1.setBackground(Color.decode(blocks[j][n]));
+                area.add(h1);
+            }
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            System.out.println("Hello");
+        }
+    }
+    public class EnterAction extends AbstractAction{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("hello");
+            //Picture pic = new Picture();
+            //pic.build();
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            System.exit(0);
+        }
     }
 }
