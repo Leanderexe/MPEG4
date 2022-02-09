8b74911136d5711ea08e1862a20a2739cd87d10e
LeanderHermanns <leander.hermanns@t-online.de>
2022-02-09 18:08:39 +0100

    Add the Motion Compensation.

--- src/main/java/MotionCompensation.java
+++ src/main/java/MotionCompensation.java
@@ -1,6 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MotionCompensation {
-    public MotionCompensation(int w, int h){
-        int heigth = h;
-        int wigth = w;
+    int heigth;
+    int width;
+    int blocksize;
+
+    public MotionCompensation(int w, int h,int blocksz){
+        heigth = h;
+        width = w;
+        blocksize = blocksz;
+    }
+
+    public List build_blocks(String[][] canvas){
+        List Blocks = new ArrayList();
+        for (int i = 0; i < 4; i++) {
+            for (int p = 0; p < 4; p++) {
+                String Block = "";
+                for (int j = 8 * i; j < 8 * i + blocksize; j++) {
+                    for (int n = 8 * p; n < 8 * p + blocksize; n++) {
+                        //System.out.println("hihihihgffffffffffffffffffff");
+                        Block += canvas[j][n];
+                    }
+                    Block += "\n";
+                }
+                Blocks.add(Block);
+                System.out.println(Block);
+            }
+        }
+        System.out.println(canvas.toString());
+        System.out.println(Blocks);
+        //build_blocks(Blocks, frametype);
+        return Blocks;
+    }
+
+    public String[] compare_blocks(List prev_blocks, List new_blocks){
+        String[] compensation = new String[new_blocks.size()]; // If the previous frame has a Block that is equal to a block in the next frame, the position of that block in the previous frame will be stored at the position of the block in the new frame.  If a block is not in the previous frame -1 will be added to the list.
+        for (int i = 0; i < new_blocks.size(); i++){
+            compensation[i] = "-1";
+            for (int j= 0; j < prev_blocks.size(); j++){
+                if(new_blocks.get(i).equals(prev_blocks.get(j))){
+                    compensation[i] = String.valueOf(j);
+                    j = new_blocks.size(); // break the loop.
+            }
+            }
+        }
+        return compensation;
+
     }
 }
--- src/main/java/Picture.java
+++ src/main/java/Picture.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class Picture {
+    int heigth = 32;
+    int width = 32;
+    int blocksize = 8;
 
     public void build(){
         // first Frame.
-        for (int i = 0; i < 10; i++) {
-            List Canvas = new ArrayList();
+        List Canvas = new ArrayList();
+        for (int i = 0; i < 2; i++) {
             double random_int1 = new Random().nextInt(2) - 1; // Create a random integer number between -1 and 0.
             double random_int2 = new Random().nextInt(2) - 1;
             double random_int3 = new Random().nextInt(2) - 1;
@@ -32,7 +36,7 @@ public class Picture {
             //System.out.print(Color.BLUE_BOLD);
             Window win = new Window();
             win.build_blocks(8, 32, c.get_Canvas(), "P");
-            Canvas.add(c);
+            Canvas.add(c.get_Canvas());
             //win.build_window(c.render());
             System.out.println(c.render());
             //Scanner in = new Scanner(System.in);
@@ -43,6 +47,11 @@ public class Picture {
                 System.out.println("Hellopepe" + vid.render());
             } */
         }
+
+        MotionCompensation comp = new MotionCompensation(width, heigth, blocksize);
+        List Blocks1 = comp.build_blocks((String[][]) Canvas.get(0));
+        List Blocks2 = comp.build_blocks((String[][]) Canvas.get(1));
+        System.out.println("hihihihi" + Arrays.toString(comp.compare_blocks(Blocks1, Blocks2)));
 
         // second Frame.
         VideoFrame c2 = new VideoFrame(31,31, "+");
